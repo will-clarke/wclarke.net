4b57c8b80922c19892d658541442ff0facfcf6ea
William Clarke <wmmclarke@gmail.com>
2022-02-10 20:07:34 +0000

    Better story linking

--- scripts/story-links.sh
+++ scripts/story-links.sh
@@ -3,4 +3,4 @@
 
 printf -- "---\ntitle: stories\n---\n\n" > src/stories/index.md
 
-find src/stories -type f -name "*.md" -not -name "README.md" | sed 's/src\///' | xargs -I {} echo "[{}]({})" >> src/stories/index.md
+find src/stories -type f -name "*.md" -not -name "README.md" | sed 's/src\///' | sed 's/md$/html/' | xargs -I {} printf -- "[{}]({})\n" >> src/stories/index.md
