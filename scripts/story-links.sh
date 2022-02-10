d4d081af5404ead818c5630398707084eca87952
William Clarke <wmmclarke@gmail.com>
2022-02-10 19:59:19 +0000

    Update story index page

--- scripts/story-links.sh
+++ scripts/story-links.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-printf -- "---\ntitle: stories\n---\n\n" > src/stories.md
+printf -- "---\ntitle: stories\n---\n\n" > src/stories/index.md
 
-find src/stories -type f -name "*.md" -not -name "README.md" | sed 's/src\///' | xargs -I {} echo "[{}]({})" >> src/stories.md
+find src/stories -type f -name "*.md" -not -name "README.md" | sed 's/src\///' | xargs -I {} echo "[{}]({})" >> src/stories/index.md
